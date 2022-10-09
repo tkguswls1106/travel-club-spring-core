069f7208df60d69f61726febc9bfa31e133d9456
sahyunjin <tkguswls1106@naver.com>
2022-10-09 18:36:42 +0900

    2-8강 완료. MemberServiceLogic 클래스에 생성자 주입 방법으로 MemberMapStore 과 의존관계 형성 코드 작성 완료.

--- src/main/java/io/namoosori/travelclub/spring/service/logic/MemberServiceLogic.java
+++ src/main/java/io/namoosori/travelclub/spring/service/logic/MemberServiceLogic.java
@@ -4,12 +4,19 @@ import io.namoosori.travelclub.spring.aggregate.club.CommunityMember;
 import io.namoosori.travelclub.spring.service.MemberService;
 import io.namoosori.travelclub.spring.service.sdo.MemberCdo;
 import io.namoosori.travelclub.spring.shared.NameValueList;
+import io.namoosori.travelclub.spring.store.MemberStore;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MemberServiceLogic implements MemberService {
+
+    private MemberStore memberStore;
+    // 아마 @Autowired 써도 되는듯 하다?
+    public MemberServiceLogic(MemberStore memberStore) {
+        this.memberStore = memberStore;  // 등호(=) 왼쪽의 this.memberStore은 private MemberStore memberStore;의 memberStore 이고, 등호(=) 오른쪽의 memberStore은 public MemberServiceLogic(MemberStore memberStore)의 매개변수인 memberStore 이다.
+    }
 
     @Override
     public String registerMember(MemberCdo member) {
